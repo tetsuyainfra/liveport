c61b805c99d1790a1e2380c238f06fae93d1a24a
tetsuyainfra <tetsuyainfra@gmail.com>
2019-12-08 12:00:35 +0900

    electron/ts/Main.tsをコンパイルできるようにした

---
+++ webpack.main.config.js
@@ -0,0 +1,26 @@
+const copyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
+const DashboardPlugin = require('webpack-dashboard/plugin');
+const merge = require('webpack-merge');
+const common = require('./webpack.base.js');
+
+module.exports = merge(common, {
+    entry: {
+        "main": './src/electron/ts/Main.ts'
+    },
+    output: {
+        path: __dirname + "/build",
+        filename: '[name].js'
+    },
+    module: {
+        // noParse: ['ws'] //
+    },
+    // externals: ['ws'], // https://github.com/socketio/socket.io-client/issues/933
+    externals: {
+        ws:  'ws',
+        uws: 'uws'
+    },
+    target: "electron-main",
+    plugins: [
+    ]
+});
